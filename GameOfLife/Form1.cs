38fcc8b29bd87687dec44683a12be5ca3c0d1728
BogoslovskiyDA <anmorgana.w@gmail.com>
2020-08-04 14:31:42 +0300

    version 2

--- GameOfLife/Form1.cs
+++ GameOfLife/Form1.cs
@@ -14,10 +14,7 @@ namespace GameOfLife
     {
         private Graphics graphics;
         private int resolution;
-        private bool[,] field;
-        private int rows;
-        private int cols;
-        private int currentGeneration = 0;
+        private GameEngine gameEngine;
         public Form1()
         {
             InitializeComponent();
@@ -28,89 +25,42 @@ namespace GameOfLife
             if (Timer.Enabled)
                 return;
 
-            currentGeneration = 0;
-            Text = $"Generation {currentGeneration}";
-
             nudResolution.Enabled = false;
             nudDensity.Enabled = false;
             resolution = (int)nudResolution.Value;
-            rows = pictureBox.Height / resolution;
-            cols = pictureBox.Width / resolution;
-            field = new bool[cols, rows];
 
-            Random random = new Random();
-            for (int x = 0; x < cols; x++)
-            {
-                for (int y = 0; y < rows; y++)
-                {
-                    field[x, y] = random.Next((int)nudDensity.Value) == 0;
-                }
-            }
+            gameEngine = new GameEngine
+            (
+                rows: pictureBox.Height / resolution,
+                cols: pictureBox.Width / resolution,
+                density: (int)(nudDensity.Minimum) + (int)(nudDensity.Maximum) - (int)(nudDensity.Value)
+            );
+
+            Text = $"Generation {gameEngine.CurrentGeneration}";
 
             pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
             graphics = Graphics.FromImage(pictureBox.Image);
             Timer.Start();
         }
 
-        private void NextGeneration()
+        private void DrawNextGeneration()
         {
             graphics.Clear(Color.Black);
 
-            var newfield = new bool[cols, rows];
+            var field = gameEngine.GetCurrentGeneration();
 
-            for (int x = 0; x < cols; x++)
+            for (int x = 0; x < field.GetLength(0); x++)
             {
-                for (int y = 0; y < rows; y++)
+                for (int y = 0; y < field.GetLength(1); y++)
                 {
-                    var neighboursCount = CountNeighbours(x, y);
-                    var hasLife = field[x, y];
-
-                    if (!hasLife && neighboursCount == 3)
-                        newfield[x, y] = true;
-                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3))
-                        newfield[x, y] = false;
-                    else
-                        newfield[x, y] = field[x, y];
-
-                    if (hasLife)
-                        graphics.FillRectangle(Brushes.White, x * resolution, y * resolution, resolution, resolution);
+                    if (field[x, y])
+                        graphics.FillRectangle(Brushes.White, x * resolution, y * resolution, resolution - 1, resolution - 1);
                 }
             }
 
-            //Random random = new Random();
-            //for (int x = 0; x < cols; x++)
-            //{
-            //    for (int y = 0; y < rows; y++)
-            //    {
-            //        field[x, y] = random.Next((int)nudDensity.Value) == 0;
-            //    }
-            //}
-
-            field = newfield;
             pictureBox.Refresh();
-            Text = $"Generation {++currentGeneration}";
-        }
-
-        private int CountNeighbours(int x, int y)
-        {
-            int count = 0;
-
-            for (int i = -1; i < 2; i++)
-            {
-                for (int j = -1; j < 2; j++)
-                {
-                    var col = (x + i + cols) % cols;
-                    var row = (y + j + rows) % rows;
-
-                    var isSelfChecking = col == x && row == y;
-                    var hasLife = field[col, row];
-
-                    if (hasLife && !isSelfChecking)
-                        count++;
-                }
-            }
-
-            return count;
+            Text = $"Generation {gameEngine.CurrentGeneration}";
+            gameEngine.NextGeneration();
         }
 
         private void StopGame()
@@ -124,7 +74,7 @@ namespace GameOfLife
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            NextGeneration();
+            DrawNextGeneration();
         }
 
         private void BStart_Click(object sender, EventArgs e)
@@ -146,28 +96,14 @@ namespace GameOfLife
             {
                 var x = e.Location.X / resolution;
                 var y = e.Location.Y / resolution;
-                var validationPassed = ValidateMusePosition(x, y);
-                if (validationPassed)
-                    field[x, y] = true;
+                gameEngine.AddCell(x, y);
             }
             if (e.Button == MouseButtons.Right)
             {
                 var x = e.Location.X / resolution;
                 var y = e.Location.Y / resolution;
-                var validationPassed = ValidateMusePosition(x, y);
-                if (validationPassed)
-                    field[x, y] = false;
+                gameEngine.RemoveCell(x, y);
             }
-        }
-
-        private bool ValidateMusePosition(int x, int y)
-        {
-            return x >= 0 && y >= 0 && x < cols && y < rows;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Text = $"Generation {currentGeneration}";
         }
     }
 }
